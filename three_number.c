7ffa5f651e6a0e0e94508b7805780a28445301b3
shura-gh0st69 <turtle121.mdy@gmail.com>
2025-06-09 18:20:08 +0630

    Improves code formatting
    Addresses minor formatting issues for enhanced code readability.
    
    Removes unnecessary indentation at the beginning of lines.

--- three_number.c
+++ three_number.c
@@ -1,14 +1,14 @@
-    #include <stdio.h>
+#include <stdio.h>
 
-    int main() {
-        int three = 156; // 1 5 6 
+int main() {
+    int three = 156; // 1 5 6 
 
-        int a = three / 100; // 1 
-        int b = three % 100 / 10; // three % 100 = 56 -> 56 / 10 = 5
-        int c = three % 10; // three % 10 = 6
+    int a = three / 100; // 1 
+    int b = three % 100 / 10; // three % 100 = 56 -> 56 / 10 = 5
+    int c = three % 10; // three % 10 = 6
 
-
-        printf("first number %d, second number %d, third number %d", a, b, c);
-        getchar();
-        return 0;
-    };
+    printf("first number %d, second number %d, third number %d", a, b, c);
+    
+    getchar();
+    return 0;
+};
