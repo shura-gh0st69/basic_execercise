fcf153f68ec002442cb3938e01cbcb2fda2c8ff8
shura-gh0st69 <turtle121.mdy@gmail.com>
2025-06-09 18:18:11 +0630

    Adds a program to decompose a three-digit number
    Adds a simple C program that decomposes a three-digit number
    into its individual digits and prints them to the console.

---
+++ three_number.c
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+
+    int main() {
+        int three = 156; // 1 5 6 
+
+        int a = three / 100; // 1 
+        int b = three % 100 / 10; // three % 100 = 56 -> 56 / 10 = 5
+        int c = three % 10; // three % 10 = 6
+
+
+        printf("first number %d, second number %d, third number %d", a, b, c);
+        getchar();
+        return 0;
+    };
